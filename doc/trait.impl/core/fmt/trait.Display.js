9a62b402132658faac33125babe144b792ae6039
Armin Sander <armin@replicator.org>
2024-02-16 08:36:59 +0100

    Auto-Update of /doc

---
+++ doc/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"skia_safe":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"skia_safe/svg/struct.LoadError.html\" title=\"struct skia_safe::svg::LoadError\">LoadError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
