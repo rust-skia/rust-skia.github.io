e05a75280a1e37432f3702a17d9cf136264aafd5
Armin Sander <armin@replicator.org>
2025-01-14 08:35:49 +0100

    Auto-Update of /doc

--- doc/skia_safe/resources/sidebar-items.js
+++ doc/skia_safe/resources/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ImageDecodeStrategy"],"mod":["helpers"],"struct":["LocalResourceProvider"],"trait":["ResourceProvider"],"type":["ImageAsset","NativeResourceProvider"]};
+window.SIDEBAR_ITEMS = {"enum":["ImageDecodeStrategy"],"mod":["helpers"],"struct":["LocalResourceProvider","UReqResourceProvider"],"trait":["ResourceProvider"],"type":["ImageAsset","NativeResourceProvider"]};
